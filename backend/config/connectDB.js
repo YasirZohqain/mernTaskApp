fe4e15b09348ae70d217ea385ab678bb51845641
Yasir Zohqain <yasirzohqain@gmail.com>
2024-12-17 15:14:55 +0500

    connect mongoDB trought mongoose direct in server.js file

--- backend/config/connectDB.js
+++ backend/config/connectDB.js
@@ -1,15 +1,15 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-const connectDataBase = async () => {
-    try{
-        const coonect = await mongoose.connect(process.env.MONGO_URI)
-        console.log(`MongooDB connect`)
+// const connectDataBase = async () => {
+//     try{
+//         const coonect = await mongoose.connect(process.env.MONGO_URI)
+//         console.log(`MongooDB connect`)
 
-    }
-    catch(err) {
-        console,log(err)
-        process.exit(1)
-    }
-}
+//     }
+//     catch(err) {
+//         console,log(err)
+//         process.exit(1)
+//     }
+// }
 
-module.exports = connectDataBase;
+// module.exports = connectDataBase;
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,21 @@
 const dotenv = require('dotenv').config()
 const express = require('express')
-const connectDB = require('./config/connectDB')
+//const connectDB = require('./config/connectDB')
+const mongoose = require("mongoose");
 
 const app = express()
+
+
+
+
+mongoose.connect(process.env.MONGO_URI)
+        .then(()=> {
+            app.listen(PORT,  ()=> {
+                console.log(`Server is runing on port:${PORT}`)
+            })
+        }).catch((err)=> {
+            console.log(err)
+        })
 
 
 
@@ -13,8 +26,15 @@ const app = express()
 
 
 app.get('/', (req, res)=>{
-    res.send('Home Page is on')
+    res.send('Home Page')
 })
+
+
+app.get('/about', (req, res)=>{
+    res.send('about Page OK')
+})
+
+
 
 /* express server create 
     same as here also
@@ -25,19 +45,19 @@ const PORT = process.env.PORT || 5000
 //})
 
 
-const serverStart = async () => {
-    try {
-        await connectDB();
-        app.listen(PORT,  ()=> {
-            console.log(`Server is runing on port:${PORT}`)
-        })
-    }
-    catch (err) {
-        console.log(err)
-    }
-}
+// const serverStart = async () => {
+//     try {
+//         await connectDB();
+//         app.listen(PORT,  ()=> {
+//             console.log(`Server is runing on port:${PORT}`)
+//         })
+//     }
+//     catch (err) {
+//         console.log(err)
+//     }
+// }
 
-serverStart();
+// serverStart();
 
 
 // mongodb+srv://yasirzohqain123:<db_password>@merntaskcluster.boooa.mongodb.net/?retryWrites=true&w=majority&appName=MernTaskCluster
