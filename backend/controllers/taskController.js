cb38ebfbfed7de519c7b35f8b7f22e2504a7c995
Yasir Zohqain <yasirzohqain@gmail.com>
2024-12-23 17:39:08 +0500

    read single task and study params

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -4,22 +4,36 @@ const Task = require('../models/taskModel');
 // CREATE A TASK ... craete function
 const createTask = async(req, res) => {
     try {
-        const task = await Task.create(req.body)
-        res.status(200).json(task)
-    } catch (error) {
-        res.status(500).json({msg: error.message})
-    }
-}
-
-
-
-const getTask = async(req, res) => {
-    try {
-        const tasks = await Task.find()
+        const tasks = await Task.create(req.body)
         res.status(200).json(tasks)
     } catch (error) {
         res.status(500).json({msg: error.message})
     }
 }
 
-module.exports = {createTask, getTask}
+
+
+const getTasks = async(req, res) => {
+    try {
+        const tasks = await Task.find()
+        res.status(200).json(tasks)
+    } catch (error) {
+        
+    }
+}
+
+// params
+const getTask = async (req, res) => {
+    try {
+        const {id} = req.params
+        const task = await Task.findById(id);
+        if(!task) {
+            return res.status(404).json(`NO task with id: ${id}`)
+        }
+        res.status(200).json(task)
+    } catch (error) {
+        res.status(500).json({msg: error.message})
+    }
+}
+
+module.exports = {createTask, getTasks, getTask}
--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,6 +1,6 @@
 const express = require ("express")
 const Task = require('../models/taskModel');
-const { createTask, getTask } = require("../controllers/taskController");
+const { createTask, getTasks, getTask } = require("../controllers/taskController");
 const router = express.Router()
 
 
@@ -12,9 +12,12 @@ router.post('/api/tasks', createTask)
 
 // Read Data from moongoDB 
 
-router.get('/api/tasks', getTask)
+router.get('/api/tasks', getTasks)
 
 
+// router for single task
+
+router.get('/api/tasks/:id', getTask)
 
 
 module.exports = router
