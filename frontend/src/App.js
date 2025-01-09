4984605bcae52a67a4c1004e3dee962b71b082ff
Yasir Zohqain <yasirzohqain@gmail.com>
2025-01-09 18:06:55 +0500

    components assets folder and toast code add

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,18 @@
+import TaskList from "./components/TaskList";
+import { ToastContainer, toast } from 'react-toastify';
 
 function App() {
+  const notify = () => toast("Wow so easy!");
   return (
-    <div>
-      <h1>Hello Worlds</h1>
+    <div className="app">
+      <div className="task-container">
+        <h1>OK</h1>
+        <TaskList/>
+        <div>
+        <button onClick={notify}>Notify!</button>
+        <ToastContainer />
+      </div>
+      </div>
     </div>
   );
 }
